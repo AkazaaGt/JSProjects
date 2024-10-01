05fda5577ebe8fa1d4ce8f718be6b5972dd65e2f
Vadim <shvetsov-1980@inbox.ru>
2024-10-01 21:12:52 +1000

    changeup filter and select

--- filter.js
+++ filter.js
@@ -2,11 +2,6 @@ import getListTemplate from './list.js'
 
 // TODO: improve btn filter and 
 // TODO: implement selector on breed
-const FILTER_TYPES = {
-    natural: 'Natural',
-    mutation: 'Mutation'
-}
-
 
 export default async function filter({btns, data, parent, select}) {
     if(!btns) return
@@ -14,25 +9,6 @@ export default async function filter({btns, data, parent, select}) {
 
     let currentFilter = new Map();
 
-    const handleBtnFilter = (event) => {
-        event.preventDefault()
-        const btn = event.target
-        const filterType = btn.dataset.btn
-        if(filterType === FILTER_TYPES.natural) {
-            filteredData = naturalDataFilter()
-        } else if(filterType === FILTER_TYPES.mutation) {
-            filteredData = mutationDataFilter()
-        }
-        return filteredData
-    } 
-    const handleSelectFilter = (event) =>{
-        const filterType = event.target.value
-        filteredData = data.filter(item=>{
-            return item.country === filterType;
-        })
-        console.log(filterType)
-        return filteredData;
-    }
     const handleFilter = (e) => {
         let filteredData = data
         if (e.target.dataset.btn) {
@@ -60,10 +36,6 @@ export default async function filter({btns, data, parent, select}) {
 						(item) => item.country === currentFilter.get('selectors')
 					)
 				}
-
-
- 
-    // const selectDataFilter = ()
 
     btns.forEach(btn => document.addEventListener('click', async(e) => {
         const filteredData = handleFilter(e)
